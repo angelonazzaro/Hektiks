eb69adbc400f0b0b66116f18d34bdcedea3f0640
Francescoo45 <granoziofrancesco.1@outlook.it>
2022-04-25 16:35:33 +0200

    Aggiunto fetchUtenti

--- Hektiks/src/main/java/Model/Utente/SQLUtenteDAO.java
+++ Hektiks/src/main/java/Model/Utente/SQLUtenteDAO.java
@@ -1,9 +1,14 @@
 package Model.Utente;
 
+import Model.Storage.QueryBuilder;
 import Model.Storage.SQLDAO;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,7 +21,26 @@ public class SQLUtenteDAO extends SQLDAO implements UtenteDAO<SQLException> {
 
     @Override
     public List<Utente> fetchUtenti(int start, int end) throws SQLException {
-        return null;
+
+        List<Utente> utenti = new ArrayList<>();
+
+        try(Connection conn = source.getConnection()){
+
+            String query = QueryBuilder.SELECT("*").FROM("Utenti").toString();
+            try(PreparedStatement ps = conn.prepareStatement(query)){
+
+                ps.setInt(1, start);
+                ps.setInt(2, end);
+                ResultSet set = ps.executeQuery();
+                UtenteExtractor utenteExtractor = new UtenteExtractor();
+
+                while (set.next()){
+
+                    utenti.add(utenteExtractor.extract(set));
+                }
+            }
+        }
+        return utenti;
     }
 
     @Override
