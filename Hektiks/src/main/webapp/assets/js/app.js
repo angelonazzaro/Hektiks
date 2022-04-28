34b053bbda250ace3166706332c49bac9ad2608c
sl1mSha4dey <mutenssj@gmail.com>
2022-04-28 17:03:37 +0200

    santo github

--- Hektiks/src/main/webapp/assets/js/app.js
+++ Hektiks/src/main/webapp/assets/js/app.js
@@ -1,31 +1,32 @@
-const eyeIcons = document.querySelectorAll("i.fas.fa-eye");
 const burger = document.getElementById("burger");
-const loginRegisterContainer = document.getElementById("login-register-container");
+const login_reg_section = document.getElementById("login-registration");
 
 burger.addEventListener("click", () => {
-	burger.classList.toggle("active");
-	loginRegisterContainer.classList.toggle("active");
-})
-
-eyeIcons.forEach((eyeIcon) => {
-	eyeIcon.addEventListener("click", () => {
-
-		let addType = "text", removeType = "password";
-		let addIconClass = "fa-eye-slash", removeIconClass = "fa-eye";
-		let tmpClass, tmpType;
-
-		if (!eyeIcon.classList.contains("fa-eye")) {
-			tmpType = addType;
-			addType = removeType;
-			removeType = tmpType;
-
-			tmpClass = addIconClass;
-			addIconClass = removeIconClass;
-			removeIconClass = tmpClass;
-		}
-
-		eyeIcon.classList.remove(removeIconClass);
-		eyeIcon.classList.add(addIconClass);
-		eyeIcon.parentElement.querySelector(`input[type=${removeType}]`).setAttribute("type", addType);
-	})
+    burger.classList.toggle("active")
+    if (login_reg_section) login_reg_section.classList.toggle("active");
 });
+
+const go_to_login = document.querySelector(".login-registration-form__btn[data-form=login]");
+const go_to_reg = document.querySelector(".login-registration-form__btn[data-form=registration]");
+
+if (go_to_login && go_to_reg) {
+
+    const regForm = go_to_login.parentElement;
+    const loginForm = go_to_reg.parentElement;
+
+    go_to_login.addEventListener("click", () => {
+        regForm.classList.remove("show");
+        regForm.classList.add("hide");
+
+        loginForm.classList.remove("hide");
+        loginForm.classList.add("show");
+    })
+
+    go_to_reg.addEventListener("click", () => {
+        loginForm.classList.remove("show");
+        loginForm.classList.add("hide");
+
+        regForm.classList.remove("hide");
+        regForm.classList.add("show");
+    })
+}
