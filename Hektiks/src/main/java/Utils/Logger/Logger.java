18d976b8c4c8bbb329ec849ede18095aa6cb11e7
Francescoo45 <granoziofrancesco.1@outlook.it>
2022-06-15 16:04:34 +0200

    aggiunto logger

--- Hektiks/src/main/java/Utils/Logger/Logger.java
+++ Hektiks/src/main/java/Utils/Logger/Logger.java
@@ -2,8 +2,8 @@ package Utils.Logger;
 
 public class Logger {
 
-    public static String INFO = "INFO";
-    public static String WARNING = "WARNING";
+    public static final String INFO = "INFO";
+    public static final String WARNING = "WARNING";
 
     public static void consoleLog(String logType, String message) {
         System.out.println("[" + logType +  "] " + "[" + message +"]");
